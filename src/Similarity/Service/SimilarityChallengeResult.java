2cddf0f0f1ea1bdd8a659dd000def7a140debafc
Gökhan Ercan <gokhan@ercannet.com>
2018-07-22 23:22:46 +0300

    code formatting

--- src/Similarity/Service/SimilarityChallengeResult.java
+++ src/Similarity/Service/SimilarityChallengeResult.java
@@ -1,10 +1,7 @@
 package Similarity.Service;
 
-import Corpus.Sentence;
 import Similarity.Model.SentenceSimilarityPair;
 import Similarity.QType;
-import Similarity.Sentences;
-import javafx.util.Pair;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -16,8 +13,5 @@ public class SimilarityChallengeResult implements Serializable {
 
     public ArrayList<SentenceSimilarityPair> Pairs = new ArrayList<>();
     public QType Question;
-
-    //public ArrayList<Sentences> Segments = new ArrayList<>();
-    //TODO: Segments eklenecek.
 
 }
